f49621f2576310a16bb18d048a0b069bc77117d7
Eitaro Fukamachi <e.arrows@gmail.com>
2015-09-09 04:51:44 +0900

    Add select-dao.

--- src/mito.lisp
+++ src/mito.lisp
@@ -22,18 +22,27 @@
                 #:dao-table-class
                 #:dao-synced)
   (:import-from #:mito.util
-                :unlispify)
+                #:lispify
+                #:unlispify)
   (:import-from #:dbi
                 #:with-transaction
-                #:do-sql)
+                #:do-sql
+                #:prepare
+                #:execute
+                #:fetch-all)
   (:import-from #:sxql
                 #:*quote-character*
                 #:yield
                 #:insert-into
                 #:update
                 #:delete-from
+                #:select
+                #:from
                 #:where
+                #:add-child
                 #:make-clause)
+  (:import-from #:sxql.sql-type
+                #:sql-statement)
   (:export #:table-class
            #:table-column-class
            #:table-name
@@ -49,16 +58,70 @@
            #:create-dao
            #:update-dao
            #:delete-dao
-           #:save-dao))
+           #:save-dao
+           #:select-dao))
 (in-package :mito)
 
-(defun execute-sxql (sxql)
-  (check-connected)
-  (let ((sxql:*quote-character* (or sxql:*quote-character*
-                                    (connection-quote-character *connection*))))
-    (multiple-value-bind (sql binds)
-        (sxql:yield sxql)
-      (apply #'dbi:do-sql *connection* sql binds))))
+(defmacro with-quote-char (&body body)
+  `(let ((sxql:*quote-character* (or sxql:*quote-character*
+                                     (connection-quote-character *connection*))))
+     ,@body))
+
+(defgeneric execute-sql (sql &optional binds)
+  (:method :before (sql &optional binds)
+    (declare (ignore sql binds))
+    (check-connected))
+  (:method ((sql string) &optional binds)
+    (apply #'dbi:do-sql *connection* sql binds))
+  (:method ((sql sql-statement) &optional binds)
+    (declare (ignore binds))
+    (with-quote-char
+      (multiple-value-bind (sql binds)
+          (sxql:yield sql)
+        (apply #'dbi:do-sql *connection* sql binds)))))
+
+(defun make-dao-instance (class &rest initargs)
+  (when (symbolp class)
+    (setf class (find-class class)))
+
+  (assert (and class
+               (typep class 'dao-table-class)))
+
+  (let ((obj (make-instance class)))
+    ;; Ignore columns which is not defined in defclass as a slot.
+    (loop with undef = '#:undef
+          for column in (database-column-slots class)
+          for val = (getf initargs (intern (symbol-name (table-column-name column)) :keyword)
+                          undef)
+          unless (eq val undef)
+            do (setf (slot-value obj (table-column-name column)) val))
+    (setf (dao-synced obj) t)
+    obj))
+
+(defgeneric retrieve-by-sql (sql &key binds as)
+  (:method :before (sql &key binds as)
+    (declare (ignore sql binds as))
+    (check-connected))
+  (:method ((sql string) &key binds as)
+    (let* ((results
+             (dbi:fetch-all
+              (apply #'dbi:execute (dbi:prepare *connection* sql)
+                     binds)))
+           (results
+             (loop for (k v) on results by #'cddr
+                   collect (lispify k)
+                   collect v)))
+      (if as
+          (mapcar (lambda (result)
+                    (apply #'make-dao-instance as result))
+                  results)
+          results)))
+  (:method ((sql sql-statement) &key binds as)
+    (declare (ignore binds))
+    (with-quote-char
+      (multiple-value-bind (sql binds)
+          (sxql:yield sql)
+        (retrieve-by-sql sql :binds binds :as as)))))
 
 (defun make-set-clause (obj)
   (apply #'sxql:make-clause :set=
@@ -77,7 +140,7 @@
     (check-connected)
     (let ((serial-key (table-serial-key (class-of obj))))
       (dbi:with-transaction *connection*
-        (execute-sxql
+        (execute-sql
          (sxql:insert-into (intern (table-name (class-of obj)) :keyword)
            (make-set-clause obj)))
         (when serial-key
@@ -100,7 +163,7 @@
       (unless primary-key
         (error "Unknown primary key in ~S." (table-name (class-of obj))))
 
-      (execute-sxql
+      (execute-sql
        (sxql:update (intern (table-name (class-of obj)) :keyword)
          (make-set-clause obj)
          (sxql:where
@@ -116,7 +179,7 @@
         (error "Unknown primary key in ~S." (table-name (class-of obj))))
 
       (prog1
-          (execute-sxql
+          (execute-sql
            (sxql:delete-from (intern (table-name (class-of obj)) :keyword)
              (sxql:where
               `(:and ,@(mapcar (lambda (key)
@@ -129,3 +192,18 @@
     (if (dao-synced obj)
         (update-dao obj)
         (insert-dao obj))))
+
+(defgeneric select-dao (class &rest expressions)
+  (:method :before (class &rest expressions)
+    (declare (ignore class expressions))
+    (check-connected))
+  (:method ((class symbol) &rest expressions)
+    (apply #'select-dao (find-class class) expressions))
+  (:method ((class dao-table-class) &rest expressions)
+    (let ((select-sql
+            (sxql:select :*
+              (sxql:from (intern (table-name class) :keyword)))))
+      (dolist (ex expressions)
+        (sxql:add-child select-sql ex))
+
+      (retrieve-by-sql select-sql :as class))))
