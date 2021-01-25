06f7c7b3ae5a5c58328fbab08b7ea71fcb2c8c8a
Eitaro Fukamachi <e.arrows@gmail.com>
2021-01-25 17:18:27 +0900

    Revert "Allow relational slot readers even for mixin classes."

--- src/core/dao/table.lisp
+++ src/core/dao/table.lisp
@@ -9,25 +9,39 @@
                 #:driver-type)
   (:import-from #:mito.class
                 #:table-class
+                #:table-name
+                #:table-column-name
                 #:table-column-type
                 #:table-column-slots
+                #:table-column-references-column
                 #:table-primary-key
-                #:create-table-sxql)
+                #:create-table-sxql
+                #:find-slot-by-name
+                #:find-child-columns)
   (:import-from #:mito.dao.column
+                #:*conc-name*
                 #:dao-table-column-class
                 #:dao-table-column-inflate)
   (:import-from #:mito.dao.mixin
-                #:dao-table-mixin
-                #:dao-class
                 #:serial-pk-mixin
                 #:uuid-pk-mixin
-                #:record-timestamps-mixin
-                #:add-relational-readers)
-  (:export #:dao-table-class
+                #:record-timestamps-mixin)
+  (:export #:dao-class
+           #:dao-table-class
+
+           #:dao-synced
+
+           #:make-dao-instance
+
            #:depending-table-classes))
 (in-package :mito.dao.table)
 
-(defclass dao-table-class (dao-table-mixin)
+(defclass dao-class ()
+  ((synced :type boolean
+           :initform nil
+           :accessor dao-synced)))
+
+(defclass dao-table-class (table-class)
   ((auto-pk :initarg :auto-pk
             :initform '(:serial))
    (record-timestamps :initarg :record-timestamps
@@ -59,6 +73,92 @@
                  (getf slot :primary-key))
                (getf initargs :direct-slots))))
 
+(defun make-dao-instance (class &rest initargs)
+  (setf class (ensure-class class))
+
+  (assert (and class
+               (typep class 'table-class)))
+
+  (let* ((list (loop for (k v) on initargs by #'cddr
+                     for column = (find-if (lambda (initargs)
+                                             (find k initargs :test #'eq))
+                                           (table-column-slots class)
+                                           :key #'c2mop:slot-definition-initargs)
+                     if column
+                       append (list k
+                                    (dao-table-column-inflate column v))
+                     else
+                       append (list k v)))
+         (obj (allocate-instance class))
+         (obj (apply #'shared-initialize obj nil list)))
+    (setf (dao-synced obj) t)
+    obj))
+
+(defun make-relational-reader-method (func-name class slot-name rel-class)
+  (let ((generic-function
+          (ensure-generic-function func-name :lambda-list '(object))))
+    (add-method
+     generic-function
+     (make-instance 'standard-method
+                    :lambda-list '(object)
+                    :qualifiers ()
+                    :specializers (list class)
+                    :function
+                    (let ((calledp nil))
+                      (lambda (object &rest ignore)
+                        (declare (ignore ignore))
+                        ;; I don't know why but SBCL pass a CONS of the instance instead of the instance itself.
+                        (when (consp object)
+                          (setf object (first object)))
+                        (if (and (slot-boundp object slot-name)
+                                 (or calledp
+                                     (not (null (slot-value object slot-name)))))
+                            (slot-value object slot-name)
+                            (let* ((child-columns (find-child-columns class
+                                                                      (find-slot-by-name class slot-name)))
+                                   (foreign-object
+                                     (and (every (lambda (slot-name)
+                                                   (and (slot-boundp object slot-name)
+                                                        (slot-value object slot-name)))
+                                                 child-columns)
+                                          (let ((result
+                                                  (first
+                                                   (mito.db:retrieve-by-sql
+                                                    (sxql:select :*
+                                                      (sxql:from (sxql:make-sql-symbol (table-name rel-class)))
+                                                      (sxql:where
+                                                       `(:and
+                                                         ,@(mapcar (lambda (slot-name)
+                                                                     `(:= ,(sxql:make-sql-symbol
+                                                                            (table-column-name
+                                                                             (table-column-references-column
+                                                                              (find-slot-by-name class slot-name))))
+                                                                          ,(slot-value object slot-name)))
+                                                                   child-columns)))
+                                                      (sxql:limit 1))))))
+                                            (and result
+                                                 (apply #'make-dao-instance rel-class result))))))
+                              (setf calledp t
+                                    (slot-value object slot-name) foreign-object)))))))))
+
+(defun add-relational-readers (class initargs)
+  (loop for column in (copy-seq (getf initargs :direct-slots)) ;; Prevent infinite-loop
+        for (col-type . not-null) = (let ((col-type (getf column :col-type)))
+                                      (optima:match col-type
+                                        ((or (list 'or :null x)
+                                             (list 'or x :null))
+                                         (cons x t))
+                                        (otherwise
+                                         (cons col-type nil))))
+        when (and (symbolp col-type)
+                  (not (null col-type))
+                  (not (keywordp col-type)))
+          do (let* ((name (getf column :name))
+                    ;; FIXME: find-class returns NIL if the class is this same class
+                    (rel-class (find-class col-type)))
+               (dolist (reader (getf column :readers))
+                 (make-relational-reader-method reader class name rel-class)))))
+
 (defun depending-table-classes (class)
   (let ((class-name (class-name class)))
     (delete-duplicates
@@ -72,7 +172,7 @@
      :test #'eq)))
 
 (defmethod initialize-instance :around ((class dao-table-class) &rest initargs
-                                        &key direct-superclasses &allow-other-keys)
+                                        &key direct-superclasses conc-name &allow-other-keys)
   (when (and (initargs-enables-record-timestamps initargs)
              (not (contains-class-or-subclasses 'record-timestamps-mixin direct-superclasses)))
     (setf (getf initargs :direct-superclasses)
@@ -96,10 +196,13 @@
                                                direct-superclasses))))
           (push (find-class auto-pk-class) (getf initargs :direct-superclasses))))))
 
-  (apply #'call-next-method class initargs))
+  (let ((*conc-name* (first conc-name)))
+    (let ((class (apply #'call-next-method class initargs)))
+      (add-relational-readers class initargs)
+      class)))
 
 (defmethod reinitialize-instance :around ((class dao-table-class) &rest initargs
-                                          &key direct-superclasses &allow-other-keys)
+                                          &key direct-superclasses conc-name &allow-other-keys)
   (when (and (initargs-enables-record-timestamps initargs)
              (not (contains-class-or-subclasses 'record-timestamps-mixin direct-superclasses)))
     (setf (getf initargs :direct-superclasses)
@@ -120,7 +223,10 @@
                                                direct-superclasses))))
           (push (find-class auto-pk-class) (getf initargs :direct-superclasses))))))
 
-  (apply #'call-next-method class initargs))
+  (let ((*conc-name* (first conc-name)))
+    (let ((class (apply #'call-next-method class initargs)))
+      (add-relational-readers class initargs)
+      class)))
 
 (defmethod c2mop:ensure-class-using-class :around ((class dao-table-class) name &rest keys
                                                    &key direct-superclasses &allow-other-keys)
