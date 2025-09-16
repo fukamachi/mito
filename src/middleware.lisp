cc98249a6e04bb9523d5052e074fa73af05607a4
Eitaro Fukamachi <e.arrows@gmail.com>
2025-09-16 22:55:27 +0900

    Add `:no-cache` option to the Lack middleware.
    Default is `t` for SQLite3, and `nil` for others.

--- src/middleware.lisp
+++ src/middleware.lisp
@@ -4,14 +4,26 @@
   (:import-from #:mito.core
                 #:*connection*)
   (:import-from #:dbi
+                #:connect
                 #:connect-cached)
   (:export #:*lack-middleware-mito*))
 (in-package #:mito.middleware)
 
 (defparameter *lack-middleware-mito*
-  (lambda (app db-config)
-    (if db-config
-        (lambda (env)
-          (let ((mito.core:*connection* (apply #'dbi:connect-cached db-config)))
-            (funcall app env)))
-        app)))
+  (lambda (app db-config &key (no-cache nil no-cache-specified-p))
+    (let* ((driver (first db-config))
+           (no-cache (cond
+                       (no-cache-specified-p no-cache)
+                       ((eq driver :sqlite3) t)
+                       (t nil))))
+      (if db-config
+          (if no-cache
+              (lambda (env)
+                (let ((mito.core:*connection* (apply #'dbi:connect db-config)))
+                  (unwind-protect
+                      (funcall app env)
+                    (dbi:disconnect mito.core:*connection*))))
+              (lambda (env)
+                (let ((mito.core:*connection* (apply #'dbi:connect-cached db-config)))
+                  (funcall app env))))
+          app))))
